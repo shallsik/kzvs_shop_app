b04aea04318432706f072bd1254b97b2e84cd8ea
shallsik <rtrtt200@gmail.com>
2024-03-26 20:16:54 +0300

    after adding fixtures

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,32 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True,  verbose_name='URL')
+    
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+        
+    def __str__(self) -> str:
+        return self.name
+        
+        
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True,  verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Картинка')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    quantity= models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category= models.ForeignKey(to=Categories, on_delete=models.PROTECT, verbose_name='Категория')
+    
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        
+    def __str__(self) -> str:
+        return f'{self.name} Количество - {self.quantity}'
--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request):
+    
+    categories = Categories.objects.all()
+
     context = {
         'title': 'КЗВС - Главная',
         'content': 'Магазин зоотоваров КЗВС',
+        'categories': categories,
+        
     }
     
     return render(request, 'main/index.html', context)
