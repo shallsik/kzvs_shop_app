3e46e05d9522d57015ab20501829fed35f0dd54b
shallsik <rtrtt200@gmail.com>
2024-03-26 20:26:50 +0300

    debug toolbar added

--- kzvsshop/settings.py
+++ kzvsshop/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    "debug_toolbar",
+    
     'main',
     'goods',
     
@@ -52,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'kzvsshop.urls'
@@ -125,6 +129,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
+
+INTERNAL_IPS = [
+
+    "127.0.0.1",
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- kzvsshop/urls.py
+++ kzvsshop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from kzvsshop.settings import DEBUG
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,11 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
     
 ]
+
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+        
+    ]
+    
