fae1638fba7a731d2192ccd7c53c45c3a1dcda2c
shallsik <rtrtt200@gmail.com>
2024-03-27 23:32:48 +0300

    slug converter for categories

--- goods/views.py
+++ goods/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
     
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+    
+    
     
     context = {
         "title": "КЗВС Каталог",
