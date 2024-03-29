e25779dd68b587072ebfead823ff9ffab66614cf
shallsik <rtrtt200@gmail.com>
2024-03-29 20:03:48 +0300

    postgresql database connected and full text search products complete

--- kzvsshop/settings.py
+++ kzvsshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     
     "debug_toolbar",
     
@@ -86,8 +87,12 @@ WSGI_APPLICATION = 'kzvsshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kzvs',
+        'USER': 'kzvs',
+        'PASSWORD': 'kzvs',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
