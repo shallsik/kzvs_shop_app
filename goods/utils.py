b87b55673fb9241380d0b33bd80e5861b50530c4
shallsik <rtrtt200@gmail.com>
2024-03-30 19:02:28 +0300

    full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,16 +1,25 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import (
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+)
 
 
 from goods.models import Products
 
 
-
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
-    vector = SearchVector("name", "description")
-    query = SearchQuery("cheese")
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    ) 
+    
+    return result
