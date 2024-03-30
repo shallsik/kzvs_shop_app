6bd11f41b998231a5932a6a884b9cfba402ee194
shallsik <rtrtt200@gmail.com>
2024-03-30 21:43:38 +0300

    login form and controller are ready

--- kzvsshop/settings.py
+++ kzvsshop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     
     'main',
     'goods',
+    'users',
     
 ]
 
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'kzvsshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,3 +150,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_image', blank=True, null=True, verbose_name='Аватар')
+    
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователи'
+        
+    def __str__(self) -> str:
+        return self.username
