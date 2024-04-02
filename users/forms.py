57a0342e68cdba3eaaf6986f5f7cefe70a36483d
shallsik <rtrtt200@gmail.com>
2024-04-02 12:33:23 +0300

    profile and only login users can connect

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
+
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -10,20 +11,46 @@ class UserLoginForm(AuthenticationForm):
         model = User
         fields = [
             'username',
-            'password'
+            'password',
         ]
-#    username = forms.CharField()
-#    password = forms.CharField()
     
-#    username = forms.CharField(
-#        label= 'Имя пользователя',
-#       widget=forms.TextInput(attrs={"autofocus": True,
-#                                     "class": "form-control",
-#                                     "placeholder": "Введите ваше имя пользователя",
-#                                      }))
-#    password = forms.CharField(
-#        label= 'Пароль',
-#        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-#                                          "class": "form-control",
-#                                          "placeholder": "Введите ваш пароль",
-#                                          }),)
+    username = forms.CharField()
+    password = forms.CharField()
+      
+        
+class UserRegistrationForm(UserCreationForm):
+    
+    class Meta:
+        model= User
+        fields = {
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        }
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+    
+    
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",)
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 
@@ -17,6 +18,11 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
+                
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                    
                 return HttpResponseRedirect(reverse('main:index'))
     else: 
         form = UserLoginForm()
@@ -31,24 +37,49 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно зарегестрированы и вошли в аккаунт")
+            return HttpResponseRedirect(reverse('main:index'))
+    else: 
+        form = UserRegistrationForm()
+    
+    
     context = {
-        'title': 'КЗВС - Регистрация'
-        
+        'title': 'КЗВС - Регистрация',
+        'form': form,
     }
     
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профиль успешно обновлен")
+            return HttpResponseRedirect(reverse('users:profile'))
+    else: 
+        form = ProfileForm(instance=request.user)
+    
+    
     context = {
-        'title': 'КЗВС - Кабинет'
-        
+        'title': 'КЗВС - Профиль',
+        'form': form,
     }
     
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
-    pass
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
+    auth.logout(request)
+    return redirect(reverse('main:index'))
 
 
