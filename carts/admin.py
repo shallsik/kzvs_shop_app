b25cb4c3dd2c7bb23ab4cfd5b915e02114d698ae
shallsik <rtrtt200@gmail.com>
2024-04-02 15:27:10 +0300

    add to cart view refactor included cart templates

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from carts.models import Cart
+
+    
+
+admin.site.register(Cart)
+
--- users/views.py
+++ users/views.py
@@ -20,8 +20,10 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
                 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('users:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
+                
                     
                 return HttpResponseRedirect(reverse('main:index'))
     else: 
